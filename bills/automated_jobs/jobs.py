243ba8c7dcefaf26636b7f4c49296ba5c40f9b87
unknown <chuksedwin.ce@gmail.com>
2022-08-05 21:27:07 +0100

    bills/automated_jobs/jobs.py updated

--- bills/automated_jobs/jobs.py
+++ bills/automated_jobs/jobs.py
@@ -5,12 +5,12 @@ from apartments.models import *
 
 def schedule_api():
     pass
-    # occupant=Occupant.objects.all()
-    # apartments=Apartment.objects.filter(status='taken')
-    # for apartment in apartments:
-    #     for c in apartment.occupant_set.all():
-    #         bill_obj=Bill.objects.create(occupant_id=c.occupant.id,status='billed',
-    #                     billed_for_id=apartment.id,amount_due=apartment.apartment_type.price,      
-    #                 )
-    # bill_obj.save()
-    # print("Bills generated")
+    occupant=Occupant.objects.all()
+    apartments=Apartment.objects.filter(status='taken')
+    for apartment in apartments:
+        for c in apartment.occupant_set.all():
+            bill_obj=Bill.objects.create(occupant_id=c.occupant.id,status='billed',
+                        billed_for_id=apartment.id,amount_due=apartment.apartment_type.price,      
+                    )
+    bill_obj.save()
+    print("Bills generated")

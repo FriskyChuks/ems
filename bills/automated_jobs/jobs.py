5604a02b3d1855fadf2456a0dae6727a1dd5b689
unknown <chuksedwin.ce@gmail.com>
2022-08-08 14:09:10 +0100

    auto billing added

--- bills/automated_jobs/jobs.py
+++ bills/automated_jobs/jobs.py
@@ -5,10 +5,10 @@ from apartments.models import *
 
 def schedule_api():
     pass
-    occupants=Occupant.objects.all()
-    for occupant in occupants:
-        bill_obj=Bill.objects.create(occupant_id=occupant.occupant.id,status='billed',
-                    billed_for_id=occupant.apartment.id,
-                    amount_due=occupant.apartment.apartment_type.price)
-        bill_obj.save()
-    print("Bills generated")
+    # occupants=Occupant.objects.all()
+    # for occupant in occupants:
+    #     bill_obj=Bill.objects.create(occupant_id=occupant.occupant.id,status='billed',
+    #                 billed_for_id=occupant.apartment.id,
+    #                 amount_due=occupant.apartment.apartment_type.price)
+    #     bill_obj.save()
+    # print("Bills generated")
